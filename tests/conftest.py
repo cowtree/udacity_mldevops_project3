cedb1f090f5ba24a501000c0d5d9b520d7c71b76
cowtree <huycaotrido@gmail.com>
2022-12-22 11:58:21 +0100

    added unit tests for ml functions and main.py for API

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,29 @@
+'''Pytest fixtures for testing.'''
 import pytest
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from ml.data import process_data
 
 # define pytest fixtures
+pytest.fixture(scope="session")
+def prep_train_data():
+    """Prepare data for machine learning model."""
+    data = pd.read_csv("/Users/moscao/Projects/udacity_mlops/project3/udacity_mldevops_project3/data/census.csv")
+    train, _ = train_test_split(data, test_size=0.20)
 
+    cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
 
-pytest.fixture(scope="module")
-def trained_model():
-    pass
+    x_train, y_train, _, _ = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+        )
+
+    return x_train, y_train
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,35 @@
+'''Unit tests for the model.py file.'''
 
+import numpy as np
+from ml.model import train_model, compute_model_metrics, inference
 
-# define tests
-def test_train_model():
-   pass
+# define unit tests of the functions in the model.py file
+def test_train_model(prep_train_data):
+    '''Test the train_model function in the model.py file.'''
+    x_train, y_train = prep_train_data()
+    model = train_model(x_train, y_train)
 
-def test_compute_model_metrics():
-   pass
+    assert model is not None
+    assert model == "RandomForestClassifier"
 
-def test_inference():
-    pass
+def test_compute_model_metrics(prep_train_data):
+    '''Test the compute_model_metrics function in the model.py file.'''
+
+    x_train, y_train = prep_train_data()
+    model = train_model(x_train, y_train)
+    preds = inference(model, x_train)
+    metrics = compute_model_metrics(y_train, preds)
+
+    assert len(metrics) == 3
+    assert isinstance(metrics) == tuple
+    for metric in metrics:
+        assert isinstance(metric, float)
+
+def test_inference(prep_train_data):
+    '''Test the inference function in the model.py file.'''
+    x_train, y_train = prep_train_data()
+    model = train_model(x_train, y_train)
+    preds = inference(model, x_train)
+    assert len(preds) == len(x_train) 
+    assert np.all((preds==0)|(preds == 1)) is True
     
